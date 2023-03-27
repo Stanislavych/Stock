b2512b08c7f9992577abf03b01dff8e2f3f8c545
Stanislavych <stasik.bartosh@mail.ru>
2023-03-27 19:05:45 +0300

    upload files

---
+++ Stock.Models/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock.Models.Models;
+
+namespace Stock.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 1,
+                Name = "Banana",
+                ItemType = "Food",
+                Manufacturer = "Belarus",
+                ManufacturerAddress = "Pinsk",
+                ReceiptDate = DateTime.Now
+            });
+        }
+    }
+}
---
+++ Stock.Models/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Stock.Models.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string ManufacturerAddress { get; set; }
+        public string ItemType { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
